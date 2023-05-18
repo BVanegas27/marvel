7828bc129a2bb42bf4fc28a6cf211f9e6eba1b45
BVanegas27 <brandonvm2723@gmail.com>
2023-05-18 07:31:02 -0500

    ajustes realizados en comic.js

--- src/pages/Comics.js
+++ src/pages/Comics.js
@@ -2,7 +2,7 @@ export default function Comics() {
     return (
         <div>
             
-            <p>Estamos en comics</p>
+            <p>Estamos en comics, promto hatemos unos cambios</p>
 
         </div>
     )
